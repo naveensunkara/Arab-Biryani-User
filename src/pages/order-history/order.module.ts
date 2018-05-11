856d82ab10625a155fe9ad4190fcda773354a413
karun kumar <karun@b2bitsolutions.com>
2018-05-11 10:40:21 +0530

    my orders

--- src/pages/order-history/order.module.ts
+++ src/pages/order-history/order.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 
 import { OrderPage } from './order';
+import { Ionic2RatingModule } from 'ionic2-rating';
 
 @NgModule({
     declarations: [
         OrderPage
     ],
     imports: [
-        IonicPageModule.forChild(OrderPage)
+        IonicPageModule.forChild(OrderPage),
+        Ionic2RatingModule 
     ],
     exports: [
         OrderPage
