ef4ba3d01cb2bc3d51e3eff65485327973a50828
karun kumar <karun@b2bitsolutions.com>
2018-05-11 07:19:44 +0530

    menu

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -5,7 +5,7 @@ export const FirstRunPage = 'WelcomePage';
 
 // The main page the user will see as they use the app over a long period of time.
 // Change this if not using tabs
-export const MainPage = 'TabsPage';
+export const MainPage = 'MenuPage';
 
 // The initial root pages for our tabs (remove if not using tabs)
 export const Tab1Root = 'ListMasterPage';
