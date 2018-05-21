35f209e6fe28d5c3520e7161dba8201aac474ece
Naveen <naveen241193@gmail.com>
2018-05-21 23:33:57 +0530

    asdsad

--- src/pages/create-ticket/create-ticket.ts
+++ src/pages/create-ticket/create-ticket.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the CreateTicketPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -14,12 +8,12 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'create-ticket.html',
 })
 export class CreateTicketPage {
-
+  level: any = 'normal';
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CreateTicketPage');
+  nextpage() {
+    this.navCtrl.push('ViewTicketPage');
   }
 
 }
